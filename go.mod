9e7f3bdd866c0543d71fe4deba35e79223590025
achoistic98 <achoistic98@naver.com>
2024-11-01 16:21:40 +0900

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test-for-home-idp
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 간단한 핸들러 함수
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	// "/hello" 경로에 대한 요청을 helloHandler로 처리
+	http.HandleFunc("/hello", helloHandler)
+
+	// 서버 시작
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Server failed:", err)
+	}
+}
